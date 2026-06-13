6252a0b72bb6ffec57b7388e71650d9d1e474eb1
starchen_ <hongwenchen1@huawei.com>
2026-06-13 09:26:51 +0800

    asys diagnose support new chip type
    Co-authored-by: starchen_<hongwenchen1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !312 merge master into master
    
    asys diagnose support new chip type
    
    Created-by: starchen_
    Commit-by: starchen_
    Merged-by: cann-robot
    Description: ## 描述
    asys diagnose支持新芯片类型
    
    ## 关联的Issue
    关联Issue [#126](https://gitcode.com/cann/oam-tools/issues/126) 
    
    ## 测试
    通过UT/ST/RDV测试
    
    ## 文档更新
    无
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [x] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    See merge request: cann/oam-tools!312

---
+++ src/asys/common/ascend910_96/ascend91096_handler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
+from common.ascend950.ascend950_handler import Ascend950Handler
+
+
+class Ascend91096Handler(Ascend950Handler):
+    """Ascend910_96 device info, interface and implementation are consistent with Ascend950"""
