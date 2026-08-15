efeb977bc327503138914e4b291d637eef85de2e
wangchuang616 <wangchuang12@huawei.com>
2026-08-15 10:00:40 +0800

    oam-tools切换CCE集群
    Co-authored-by: wangchuang616<wangchuang12@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !507 merge master into master
    
    oam-tools切换CCE集群
    
    Created-by: wangchuang616
    Commit-by: wangchuang616
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    See merge request: cann/oam-tools!507

--- .gitcode/scripts/pre_smoke.sh
+++ .gitcode/scripts/pre_smoke.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# -----------------------------------------------------------------------------------------------------------
-# Copyright (c) 2026 Huawei Technologies Co., Ltd.
-# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
-# CANN Open Software License Agreement Version 2.0 (the "License").
-# Please refer to the License for details. You may not use this file except in compliance with the License.
-# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
-# See LICENSE in the root of the software repository for the full text of the License.
-# -----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 
 set -euo pipefail
 
 echo "start run test case, please wait ..."
-cd /home/taskspace
-WORKSPACE=/home/taskspace
+cd ${WORKSPACE}
 
 export ASCEND_GLOBAL_LOG_LEVEL=2
 export ASCEND_SLOG_PRINT_TO_STDOUT=0
@@ -31,24 +36,13 @@ log "init test case, please wait ..."
 rm -rf /root/ascend/log
 
 # ==============================
-# 确定要测试的 ops 列表
-# ==============================
-declare -a ops
-ops=("is_finite")
-
-# ==============================
 # 运行测试主循环
 # ==============================
 
-for op in "${ops[@]}"; do
-  echo "Processing: $op"
-  mode="eager"
-  [ "$op" = "crop_and_resize" ] && mode="graph"
-  source /usr/local/Ascend/cann/set_env.sh
-  echo "./examples/deploy.sh"
-  pip3 install onnx
-  bash ./examples/deploy.sh 2>&1 | tee -a ${WORKSPACE}/run_test.log
-done
+source /usr/local/Ascend/cann/set_env.sh
+echo "./examples/deploy.sh"
+pip3 install onnx
+bash ./examples/deploy.sh 2>&1 | tee -a ${WORKSPACE}/run_test.log
 
 # ==============================
 # 打包log
@@ -56,11 +50,6 @@ done
 mkdir -p /root/ascend
 slog_name="slog.tar.gz"
 tar -zcf "${slog_name}" -C /root/ascend log
-
-# upload plog
-if python3 /home/upload.py --bucket-name "ascend-ci" --action upload  --local-file "slog.tar.gz" --obs-object-key "${obs_smoke_path}/${slog_name}"; then
-  echo "::set-output var=plog_url:https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${obs_smoke_path}/slog.tar.gz"
-fi
 
 # ==============================
 # 检查 NPU 状态
